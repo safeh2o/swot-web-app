b28f512cff9499f2a580ebe08f6e2da4cd4856da
Bora Bilgin <bora@bilg.in>
2019-10-16 08:29:49 -0400

    Added standardization features

--- utils/filename.js
+++ utils/filename.js
@@ -2,7 +2,8 @@ var nameFunctions = require('keystone-storage-namefunctions');
 
 exports.swotAnalysisFilenameGenerator = function (file, index, callback) {
   nameFunctions.randomFilename(file, index, function(f, filename) {
-    const originalFilename = file.originalname.replace(/[^0-9a-z]/gi, ''); //remove non alphanumeric characters and then remove file extension
-    callback(null,  `${originalFilename}_${filename}`);
+    const extension = file.originalname.indexOf(".") != -1 ? file.originalname.substr(file.originalname.lastIndexOf(".")) : ".xlsx";
+    const originalFilename = file.originalname.replace(/[^0-9a-z]/gi, ''); //remove non alphanumeric characters
+    callback(null,  `${originalFilename}_${filename}${extension}`);
   });  
 }

fa6d1590cd269b70f79cf6a6f4e4e4b5ce01ec40
Bora Bilgin <bora@bilg.in>
2019-10-20 22:12:57 -0400

    Filename related bugs fixed in standardization

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -9,6 +9,9 @@ const mailer = require('../../utils/emailer');
 const analyzer = require('../../utils/analyzer');
 const dataService = require('../../utils/data.service');
 const std = require('../../utils/standardize');
+const azure = require('azure-storage');
+const path = require('path');
+
 /**
  * Upload a new Dataset file
  */
@@ -60,25 +63,70 @@ exports.update = async function(req, res) {
 
     dataItem.getUpdateHandler(req).process(data, async function(err) {
 
+      if (err) return res.apiError('Dataset was not updated with fieldsite, please try uploading again', err);
+
+
       await associateDatasetWithUser(req.user._id, dataItem._id);
 
       await associateDatasetWithFieldsite(data.fieldsite, dataItem._id);
       
-      if (err) return res.apiError('Dataset was not updated with fieldsite, please try uploading again', err);
-
       const project = await dataService.getProjectByFieldsite(data.fieldsite);
       const country = await dataService.getCountryByProject(project._id);      
       const fieldsite = await dataService.getFieldsiteById(data.fieldsite);
+      
+      const originalFileExtension = path.extname(dataItem.file.filename);
+      const targetBlobName = `${dataService.sanitizeStr(dataItem.file.filename.replace(originalFileExtension, ""))}__${dataService.sanitizeStr(fieldsite.name)}__${dataService.sanitizeStr(data.dateOfReading)}`;
+      const stdBlobName = targetBlobName + ".csv";
+      const rawBlobName = targetBlobName + originalFileExtension;
+
+      await renameBlobFile(dataItem.file.url, rawBlobName, process.env.AZURE_STORAGE_CONTAINER);
+
+      if (process.env.STANDARDIZE_DATASET == 1) {
+        await renameBlobFile(dataItem.file.url.replace(process.env.AZURE_STORAGE_CONTAINER, process.env.AZURE_STORAGE_CONTAINER_STD).replace(originalFileExtension, ".csv"), 
+          stdBlobName,
+          process.env.AZURE_STORAGE_CONTAINER_STD);
+      }
+
+      const rawDataURL = await getBlobURL(process.env.AZURE_STORAGE_CONTAINER, rawBlobName);
+      const stdDataURL = await getBlobURL(process.env.AZURE_STORAGE_CONTAINER_STD, stdBlobName);
+      await updateDatasetWithNewBlobURLs(data._id, rawDataURL, stdDataURL);
+
       await updateDatasetWithBlobPrefix(data._id, country.name, `${dataService.getIdentifier(project)}/${dataService.getIdentifier(fieldsite)}`)
-      // dont forget to replace
-      const csvFilename = dataItem.file.filename + ".csv";
-      await analyzer.notifyFileUpload(csvFilename, req.user.email, country, project, fieldsite, req.user, dataItem);
+      
+      await analyzer.notifyFileUpload(stdBlobName, req.user.email, country, project, fieldsite, req.user, dataItem);
 
       res.apiResponse({
         file: dataItem
       });
     });
   });
+}
+
+async function getBlobURL(containerName, blobName) {
+  const retryOperations = new azure.LinearRetryPolicyFilter();
+  const blobService = azure.createBlobService().withFilter(retryOperations);
+  return new Promise((resolve, reject) => {
+    const url = blobService.getUrl(containerName, blobName)
+    resolve(url);
+  });
+}
+
+async function renameBlobFile(sourceURI, targetBlobName, containerName) {
+  const retryOperations = new azure.LinearRetryPolicyFilter();
+  const blobService = azure.createBlobService().withFilter(retryOperations);
+  return new Promise((resolve, reject) => {
+    blobService.startCopyBlob(sourceURI, containerName, targetBlobName, (err, result) => {
+      if (err) {
+        console.log(`Error occurred while renaming blobs on Azure, container name is ${containerName}, source is ${sourceURI}, target is ${targetBlobName} and error is ${err}`);
+        reject(err);
+      }
+      resolve(result);
+    })
+  });
+}
+
+async function updateDatasetWithNewBlobURLs(datasetId, rawDataURL, stdDataURL) {
+  return Dataset.model.findOneAndUpdate({_id: datasetId}, { $set: {'file.url': rawDataURL, stdDataURL: stdDataURL }}, { strict: false }).exec();
 }
 
 async function updateDatasetWithBlobPrefix(datasetId, containerName, prefix) {

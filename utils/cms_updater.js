c85a03458374f4653b6fc67aa573ac46d9979c18
Mohamed Moselhy <mohamed.moselhy@uwo.ca>
2020-06-23 14:10:48 -0400

    Allow autoupdating of blog posts if setting is present in .env
    If blog post already exists on other databases, update the content, (and author if it exists on the other databases), (and post categories, creating them if they do not exist)
    If blog post does not exist on other databases, create it using the same rules as above

--- utils/cms_updater.js
+++ utils/cms_updater.js
@@ -4,20 +4,31 @@ const mailer = require('./emailer');
 const uri = process.env.MONGO_DB_CONNECTION_STRING.replace(`/${process.env.CMS_SOURCE_DB}?`, '/?');
 
 /**
- * Update CMS on other environments through MongoDB connections
+ * Update CMS Pages on other environments through MongoDB connections
  */
 exports.updateOtherPages = async function (page) {
     if (process.env.CMS_DESTINATION_DBS && process.env.CMS_DESTINATION_DBS !== '') {
         process.env.CMS_DESTINATION_DBS.split(",").forEach(async (dbName) => {
-            await updateOther(page, dbName);
+            await updateOtherPage(page, dbName);
         });
     }
 }
 
-async function updateOther(page, dbName) {
+/**
+ * Update CMS Blogs on other environments through MongoDB connections
+ */
+exports.updateOtherBlogs = async function (blog) {
+    if (process.env.CMS_DESTINATION_DBS && process.env.CMS_DESTINATION_DBS !== '') {
+        process.env.CMS_DESTINATION_DBS.split(",").forEach(async (dbName) => {
+            await updateOtherBlog(blog, dbName);
+        });
+    }
+}
+
+async function updateOtherPage(page, dbName) {
     return MongoClient.connect(uri, {useUnifiedTopology: true}, async (err, client) => {
         if (err) {
-            mailer.emailAdmin(`Error occurred while updating CMS for ${dbName}, error is ${err}`);
+            mailer.emailAdmin(errMessage(err, 'Page', dbName));
         }
 
         const dbo = client.db(dbName);
@@ -31,7 +42,7 @@ async function updateOther(page, dbName) {
         if (pageExists) {
             pagesCollection.updateOne(query, newValues, (err, res) => {
                 if (err) {
-                    mailer.emailAdmin(`Error occurred while updating CMS for ${dbName}, error is ${err}`);
+                    mailer.emailAdmin(errMessage(err, 'Page', dbName));
                 }
 
                 console.log(`page "${page.title}" in ${dbName} updated`);
@@ -43,10 +54,52 @@ async function updateOther(page, dbName) {
         else {
             pagesCollection.insertOne(clonePage(page), function(err, res){
                 if (err) {
-                    mailer.emailAdmin(`Error occurred while updating CMS for ${dbName}, error is ${err}`);
+                    mailer.emailAdmin(errMessage(err, 'Page', dbName));
                 }
 
                 console.log(`page "${page.title}" in ${dbName} created`);
+                client.close();
+                return res;
+            });
+        }
+    });
+}
+
+async function updateOtherBlog(blog, dbName) {
+    return MongoClient.connect(uri, {useUnifiedTopology: true}, async (err, client) => {
+        if (err) {
+            mailer.emailAdmin(errMessage(err, 'Blog', dbName));
+        }
+
+        const dbo = client.db(dbName);
+        const query = { slug: blog.slug };
+        const newBlog = cloneBlog(blog);
+        await updateBlog(client, blog, newBlog, dbName);
+        const newValues = { $set: newBlog };
+
+        const blogCollection = dbo.collection('posts');
+
+        const postExists = await blogCollection.findOne(query);
+
+        if (postExists) {
+            blogCollection.updateOne(query, newValues, (err, res) => {
+                if (err) {
+                    mailer.emailAdmin(errMessage(err, 'Blog', dbName));
+                }
+
+                console.log(`blog "${blog.title}" in ${dbName} updated`);
+                client.close();
+                return res;
+            });
+        }
+
+        else {
+            blogCollection.insertOne(newBlog, function(err, res){
+                if (err) {
+                    mailer.emailAdmin(errMessage(err, 'Blog', dbName));
+                }
+
+                console.log(`blog "${blog.title}" in ${dbName} created`);
                 client.close();
                 return res;
             });
@@ -64,4 +117,71 @@ function clonePage(page) {
     }
 
     return newPage;
+}
+
+function cloneBlog(blog) {
+    const newBlog = {
+        updatedAt: blog.updatedAt,
+        slug: blog.slug,
+        title: blog.title,
+        state: blog.state,
+        __v: 0,
+        content: blog.content,
+        categories: [],
+        publishedDate: blog.publishedDate,
+        author: null
+    }
+
+    return newBlog;
+}
+
+async function updateBlog(client, srcBlog, destBlog, dbname_dest) {
+    const dbo_dest = client.db(dbname_dest);
+
+    if (srcBlog.categories && srcBlog.categories.length) {
+        const collectionName = 'postcategories';
+        const postcategories_dest = dbo_dest.collection(collectionName);
+        srcBlog.categories.forEach(async (category) => {
+            const translation = await translateModel(client, process.env.CMS_SOURCE_DB, dbname_dest, collectionName, category, 'key');
+            if (translation.dest) {
+                destBlog.categories.push(translation.dest._id);
+            }
+            else {
+                const newCat = await postcategories_dest.insertOne(translation.src);
+                destBlog.categories.push(newCat.insertedId);
+            }
+        });
+    }
+
+    if (srcBlog.author) {
+        const translation = await translateModel(client, process.env.CMS_SOURCE_DB, dbname_dest, 'users', srcBlog.author, 'email');
+        if (translation.dest) {
+            destBlog.author = translation.dest._id;
+        }
+    }
+
+    
+}
+
+async function translateModel(client, dbname_src, dbname_dest, collectionName, id, keyName) {
+    const dbo_src = client.db(dbname_src);
+    const dbo_dest = client.db(dbname_dest);
+    const collection_src = dbo_src.collection(collectionName);
+    const collection_dest = dbo_dest.collection(collectionName);
+
+    const doc_src = await collection_src.findOne({ _id: id }, { projection: { _id: 0 } });
+    const doc_dest = await collection_dest.findOne({ [keyName]: doc_src[keyName] });
+
+    return new ModelTranslation(doc_src, doc_dest);
+}
+
+function errMessage(err, collection, dbName){
+    return `Error updating CMS ${collection} on ${dbName}. Message: ${err}`;
+}
+
+class ModelTranslation {
+    constructor(src, dest) {
+        this.src = src;
+        this.dest = dest;
+    }
 }

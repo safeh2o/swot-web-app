70659baf98b0c071462b582844c295bd5193cd54
Mohamed Moselhy <mohamed.moselhy@uwo.ca>
2020-08-20 14:20:35 -0400

    Show notification on Result table changes

--- utils/analyzer.js
+++ utils/analyzer.js
@@ -9,7 +9,8 @@ const dataService = require('./data.service');
  */
 exports.notifyFileUpload = async function (filename, recipientEmail, country, project, fieldsite, user, dataset) {
   console.log(`Notifying analyzer for country/project/fieldsite/filename/dataset=${dataService.getIdentifier(country)}/${dataService.getIdentifier(project)}/${dataService.getIdentifier(fieldsite)}/${filename}/${dataset.id} uploaded by ${recipientEmail}`); 
-  return client.get(`${process.env.ANALYZER_URL}/analysis?filename=${filename}&recipient=${recipientEmail}&country=${dataService.getIdentifier(country)}&project=${dataService.getIdentifier(project)}&fieldsite=${dataService.getIdentifier(fieldsite)}&dataset=${dataset.id}`)
+  const uri = `${process.env.ANALYZER_URL}/analysis?filename=${filename}&recipient=${recipientEmail}&country=${dataService.getIdentifier(country)}&project=${dataService.getIdentifier(project)}&fieldsite=${dataService.getIdentifier(fieldsite)}&dataset=${dataset.id}`;
+  return client.get(uri)
     .on('error', function(e) {
         console.log("Error while communicating with analyzer: " + e.message);
         mailer.emailAdmin(`An error occurred while notifying the analyzer application. Error message is ${e}. User is ${user.first} ${user.last}, country is ${country}, project is ${project} and dataset is ${dataset}`);

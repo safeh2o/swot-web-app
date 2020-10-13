cf2b787d39e8dc0ba5e0d4f8c77e0ce22e68486a
Mohamed Moselhy <mohamed.moselhy@uwo.ca>
2020-10-13 03:27:23 -0400

    trello_78

---
+++ routes/views/signin.js
@@ -0,0 +1,36 @@
+var keystone = require('keystone');
+
+exports = module.exports = function(req, res) {
+	
+	if (req.user) {
+		return res.redirect('/');
+	}
+	
+	var view = new keystone.View(req, res),
+		locals = res.locals;
+	
+	locals.section = 'signin';
+ 	
+	view.on('post', function(next) {
+		
+		if (!req.body.email || !req.body.password) {
+			req.flash('error', 'Please enter your email and password.');
+			return next();
+		}
+		
+		var onSuccess = function() {
+			res.redirect('/');
+		}
+		
+		var onFail = function() {
+			req.flash('error', 'Input credentials were incorrect, please try again.');
+			return next();
+		}
+		
+		keystone.session.signin({ email: req.body.email, password: req.body.password }, req, res, onSuccess, onFail);
+		
+	});
+	
+	view.render('signin');
+	
+}

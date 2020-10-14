dbdf9cb3cc40029c645543c2914d08813fba5c85
Mohamed Moselhy <mohamed.moselhy@uwo.ca>
2020-10-14 04:56:34 -0400

    Added Google recaptcha verification to prevent spammers from using Contact forms

--- routes/views/contact.js
+++ routes/views/contact.js
@@ -1,5 +1,7 @@
-var keystone = require('keystone');
-var Enquiry = keystone.list('Enquiry');
+const keystone = require('keystone');
+const Enquiry = keystone.list('Enquiry');
+const https = require('https');
+const querystring = require('querystring');
 
 exports = module.exports = function (req, res) {
 
@@ -12,25 +14,65 @@ exports = module.exports = function (req, res) {
 	locals.formData = req.body || {};
 	locals.validationErrors = {};
 	locals.enquirySubmitted = false;
+	locals.grecaptcha = process.env.RECAPTCHA_SITE_KEY;
 
 	// On POST requests, add the Enquiry item to the database
 	view.on('post', { action: 'contact' }, function (next) {
 
-		var newEnquiry = new Enquiry.model();
-		var updater = newEnquiry.getUpdateHandler(req);
+		const newEnquiry = new Enquiry.model();
+		const updater = newEnquiry.getUpdateHandler(req);
 
-		updater.process(req.body, {
-			flashErrors: true,
-			fields: 'name, email, phone, reason, message',
-			errorMessage: 'There was a problem submitting your enquiry:',
-		}, function (err) {
-			if (err) {
-				locals.validationErrors = err.detail;
-			} else {
-				locals.enquirySubmitted = true;
-			}
-			next();
+		const postData = querystring.stringify({
+			'secret': process.env.RECAPTCHA_SECRET_KEY,
+			'response': req.body['g-recaptcha-response']
 		});
+
+		const headers = {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': postData.length
+		}
+
+		const options = {
+			hostname: 'www.google.com',
+			path: '/recaptcha/api/siteverify',
+			port: 443,
+			method: 'POST',
+			headers: headers
+		}
+
+		const captchaReq = https.request(options, (captchaRes) => {
+			captchaRes.on('data', (d) => {
+				const captchaResData = JSON.parse(d);
+				if (captchaResData.success) {
+					updater.process(req.body, {
+						flashErrors: true,
+						fields: 'name, email, reason, message',
+						errorMessage: 'There was a problem submitting your enquiry:',
+					}, function (err) {
+						if (err) {
+							locals.validationErrors = err.detail;
+							// next();
+						} else {
+							locals.enquirySubmitted = true;
+						}
+						next();
+					});
+				}
+				else {
+					req.flash('error', "Invalid captcha token, please try again.");
+					next();
+				}
+			});
+		});
+
+		captchaReq.on('error', (e) => {
+			console.error(e);
+		});
+
+		captchaReq.write(postData);
+
+		captchaReq.end();
+
 	});
 
 	view.render('contact');

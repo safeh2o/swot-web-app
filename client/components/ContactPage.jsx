fa02bfe9592d28506b3fd7cef7576a8e3cd875f3
Mohamed Moselhy <mohamed.moselhy@uwo.ca>
2021-03-22 15:16:33 -0400

    serve malsup through https

--- client/components/ContactPage.jsx
+++ client/components/ContactPage.jsx
@@ -227,7 +227,7 @@ class ContactPage extends Component {
 						async
 						defer
 					></script>
-					<script src="http://malsup.github.com/jquery.form.js"></script>
+					<script src="https://malsup.github.com/jquery.form.js"></script>
 				</Helmet>
 				<div className="container">
 					<div className=" px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">

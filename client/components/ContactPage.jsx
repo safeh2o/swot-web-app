34a3be380e08b45ea574def79b17cff3037cc2f1
Mohamed Moselhy <mohamed.moselhy@uwo.ca>
2021-03-24 15:23:23 -0400

    serve jquery form statically

--- client/components/ContactPage.jsx
+++ client/components/ContactPage.jsx
@@ -227,7 +227,6 @@ class ContactPage extends Component {
 						async
 						defer
 					></script>
-					<script src="https://malsup.github.com/jquery.form.js"></script>
 				</Helmet>
 				<div className="container">
 					<div className=" px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">

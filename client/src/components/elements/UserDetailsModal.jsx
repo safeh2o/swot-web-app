b2b74a64e0f82e10fdb43613d9142bdc64f26e4f
Mohamed Moselhy <look4d@gmail.com>
2021-11-14 20:58:03 -0500

    added change password option

--- client/src/components/elements/UserDetailsModal.jsx
+++ client/src/components/elements/UserDetailsModal.jsx
@@ -1,193 +1,14 @@
-// import { useState, useRef } from "react";
-// import { makeStyles } from "@mui/styles";
-import { IconButton } from "@mui/material";
-// import { useDispatch, useSelector } from "react-redux";
-// import { userSelectors } from "../../reducers/user";
+import {
+	Accordion,
+	AccordionDetails,
+	AccordionSummary,
+	DialogContentText,
+	IconButton,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { handleServerMessages } from "../../reducers/notifications";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-
-// const useStyles = makeStyles((theme) => ({
-// 	root: {
-// 		backgroundColor: "rgba(255,255,255,0.1)",
-// 	},
-// 	modal: {
-// 		backgroundColor: "rgba(255,255,255,0.1)",
-// 	},
-// 	paper: {
-// 		boxShadow: theme.shadows[5],
-// 		backgroundColor: "rgba(255,255,255,0.1)",
-// 	},
-// }));
-
-// export default function UserDetailsModal() {
-// 	const user = useSelector(userSelectors.user);
-// 	const classes = useStyles();
-// 	const userForm = useRef(null);
-// 	const [open, setOpen] = useState(false);
-
-// 	const handleSubmit = (e) => {
-// 		e.preventDefault();
-// 		const data = new FormData(userForm.current);
-// 		fetch("/api/user/update", {
-// 			method: "POST",
-// 			body: data,
-// 		})
-// 			.then((r) => r.json())
-// 			.then((res) => {
-// 				dispatch(handleServerMessages(res.messages));
-// 			});
-// 	};
-
-// 	const handleOpen = () => {
-// 		setOpen(true);
-// 	};
-
-// 	const handleClose = () => {
-// 		setOpen(false);
-// 	};
-
-// 	return (
-// 		<>
-// 			<IconButton
-// 				color="inherit"
-// 				// className="nav-item nav-profile profile"
-// 				onClick={handleOpen}
-// 				tabIndex="-1"
-// 				title="User Profile"
-// 			>
-// 				<AccountCircleIcon />
-// 			</IconButton>
-// 			<Modal
-// 				aria-labelledby="modal-title"
-// 				className={classes.modal}
-// 				open={open}
-// 				onClose={handleClose}
-// 				closeAfterTransition
-// 				BackdropComponent={Backdrop}
-// 				BackdropProps={{
-// 					timeout: 500,
-// 				}}
-// 			>
-// 				<Fade in={open}>
-// 					<div className="modal-content">
-// 						<div className="modal-header">
-// 							<span className="modal-title" id="modal-title">
-// 								User Details
-// 							</span>
-// 							<button
-// 								type="button"
-// 								className="close"
-// 								data-dismiss="modal"
-// 							>
-// 								&times;
-// 							</button>
-// 						</div>
-
-// 						<div className="modal-body">
-// 							<form
-// 								ref={userForm}
-// 								className="form"
-// 								role="form"
-// 								autoComplete="off"
-// 								id="formUser"
-// 								onSubmit={handleSubmit}
-// 								noValidate
-// 							>
-// 								<div className="form-group">
-// 									<label htmlFor="firstName">
-// 										First Name
-// 									</label>
-// 									<input
-// 										type="text"
-// 										autoComplete="given-name"
-// 										className="form-control form-control-lg"
-// 										name="firstName"
-// 										id="firstName"
-// 										required=""
-// 										defaultValue={user.name.first}
-// 									/>
-// 								</div>
-
-// 								<div className="form-group">
-// 									<label htmlFor="lastName">Last Name</label>
-// 									<input
-// 										type="text"
-// 										autoComplete="family-name"
-// 										className="form-control form-control-lg"
-// 										name="lastName"
-// 										id="lastName"
-// 										defaultValue={user.name.last}
-// 										required=""
-// 									/>
-// 								</div>
-
-// 								<div className="form-group">
-// 									<label htmlFor="email">Email</label>
-// 									<input
-// 										type="text"
-// 										autoComplete="email"
-// 										className="form-control form-control-lg"
-// 										name="email"
-// 										id="email"
-// 										defaultValue={user.email}
-// 										required=""
-// 									/>
-// 								</div>
-
-// 								<h6>Change Password</h6>
-// 								<div className="form-group">
-// 									<label htmlFor="password1">Password</label>
-// 									<input
-// 										type="password"
-// 										autoComplete="new-password"
-// 										className="form-control form-control-lg"
-// 										name="password1"
-// 										id="password1"
-// 										minLength="6"
-// 									/>
-// 								</div>
-
-// 								<div className="form-group">
-// 									<label htmlFor="password2">
-// 										Confirm Password
-// 									</label>
-// 									<input
-// 										type="password"
-// 										autoComplete="new-password"
-// 										className="form-control form-control-lg"
-// 										name="password2"
-// 										id="password2"
-// 										minLength="6"
-// 									/>
-// 								</div>
-
-// 								<div className="form-group pt-4">
-// 									<button
-// 										className="btn btn-danger btn-lg"
-// 										data-dismiss="modal"
-// 										aria-hidden="true"
-// 										type="reset"
-// 										onClick={handleClose}
-// 									>
-// 										Cancel
-// 									</button>
-// 									<button
-// 										className="btn btn-primary btn-lg float-right"
-// 										id="btnSaveUserDetails"
-// 										type="submit"
-// 										onClick={handleClose}
-// 									>
-// 										Save
-// 									</button>
-// 								</div>
-// 							</form>
-// 						</div>
-// 					</div>
-// 				</Fade>
-// 			</Modal>
-// 		</>
-// 	);
-// }
+import { styled } from "@mui/styles";
 
 import * as React from "react";
 import Button from "@mui/material/Button";
@@ -199,6 +20,7 @@ import DialogTitle from "@mui/material/DialogTitle";
 import useForm from "../../hooks/useForm";
 import { useDispatch, useSelector } from "react-redux";
 import { getUser, userSelectors } from "../../reducers/user";
+import PaperComponent from "./PaperComponent";
 
 export default function UserDetailsModal() {
 	const [open, setOpen] = React.useState(false);
@@ -248,8 +70,19 @@ export default function UserDetailsModal() {
 			<IconButton color="inherit" onClick={handleClickOpen}>
 				<AccountCircleIcon />
 			</IconButton>
-			<Dialog open={open} onClose={handleClose}>
-				<DialogTitle>Edit Profile</DialogTitle>
+
+			<Dialog
+				open={open}
+				onClose={handleClose}
+				PaperComponent={PaperComponent}
+				aria-labelledby="draggable-dialog-title"
+			>
+				<DialogTitle
+					id="draggable-dialog-title"
+					style={{ cursor: "move" }}
+				>
+					Edit Profile
+				</DialogTitle>
 				<DialogContent>
 					<TextField
 						autoFocus
@@ -282,6 +115,35 @@ export default function UserDetailsModal() {
 						value={state.email}
 						onChange={getTextChangeHandler("email")}
 					/>
+					<br />
+					<br />
+					<Accordion elevation={0}>
+						<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+							Change Password
+						</AccordionSummary>
+						<AccordionDetails>
+							<TextField
+								margin="dense"
+								id="password1"
+								label="New Password"
+								type="password"
+								fullWidth
+								variant="standard"
+								value={state.password1}
+								onChange={getTextChangeHandler("password1")}
+							/>
+							<TextField
+								margin="dense"
+								id="password2"
+								label="Confirm Password"
+								type="password"
+								fullWidth
+								variant="standard"
+								value={state.password2}
+								onChange={getTextChangeHandler("password2")}
+							/>
+						</AccordionDetails>
+					</Accordion>
 				</DialogContent>
 				<DialogActions>
 					<Button onClick={handleClose}>Cancel</Button>

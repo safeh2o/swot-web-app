02e5826324b9cc894bae24fbc339cfb4f7d083c2
bhekuwenza <bhekuwenza@gmail.com>
2021-12-17 20:09:41 -0500

    Contact Page

--- client/src/components/ContactPage.jsx
+++ client/src/components/ContactPage.jsx
@@ -1,52 +1,47 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import _ from "lodash";
-import AppContext from "../contexts/AppContext";
-import NoteLine from "./elements/NoteLine";
-import useForm from "../hooks/useForm";
+
 import {
-	Grid,
 	Button,
 	FormControl,
 	FormHelperText,
+	InputLabel,
 	MenuItem,
 	Select,
 	TextField,
-	Typography,
 } from "@mui/material";
-import { makeStyles } from "@mui/styles";
-import axios from "axios";
-import { useDispatch } from "react-redux";
 import {
-	addError,
-	addNotice,
+	Grid,
+	Box,
+	Card,
+	CardHeader,
+	Divider,
+	CardContent,
+} from "@mui/material";
+
+// import _ from 'lodash';
+import axios from "axios";
+
+import AppContext from "../contexts/AppContext";
+import useForm from "../hooks/useForm";
+import {
+	// addError,
+	// addNotice,
 	handleServerMessages,
 	setLoading,
 } from "../reducers/notifications";
 import { pushView } from "../reducers/view";
 
-const useStyles = makeStyles((theme) => ({
-	root: {
-		"& > *": {
-			margin: theme.spacing(1),
-		},
-	},
-	formControl: {
-		margin: theme.spacing(1),
-		minWidth: 120,
-	},
-	form: {
-		flexDirection: "column",
-		display: "flex",
-		width: "50%",
-	},
-}));
+import NotificationLine from "./elements/NotificationLine";
 
 export default function ContactPage(props) {
 	const { grecaptchaSiteKey } = useContext(AppContext);
 	const [contactReasons, setContactReasons] = useState([]);
 	const [disabled, setDisabled] = useState(true);
-	const classes = useStyles();
 	const { state, reset, getTextChangeHandler } = useForm({
 		name: "",
 		email: "",
@@ -90,139 +85,202 @@ export default function ContactPage(props) {
 		});
 	}
 
+	// Styles
+	const css = {
+		form: {
+			'& [type="text"], & [type="email"], & [type="phone"], & [type="text"], & .MuiTextField-root':
+				{
+					backgroundColor: "#f9f9f9",
+				},
+			"& button": { textTransform: "capitalize" },
+			"& #btnSubmit": {
+				color: "white",
+				mb: 1,
+			},
+		},
+	};
+
 	return (
 		<>
-			<form>
-				<section className="content-window rich-text">
-					<header>
-						<div className="content-window-title txt-condensed">
-							Contact Us
-						</div>
-					</header>
-					<section>
-						<div className={classes.form}>
-							<FormControl className={classes.formControl}>
-								<TextField
-									required
-									label="Name"
-									value={state.name}
-									id="name"
-									onChange={getTextChangeHandler("name")}
-								/>
-								<FormHelperText>
-									So we can address you
-								</FormHelperText>
-							</FormControl>
-							<FormControl className={classes.formControl}>
-								<TextField
-									id="email"
-									type="email"
-									label="Email"
-									value={state.email}
-									required
-									onChange={getTextChangeHandler("email")}
-								/>
-								<FormHelperText>
-									We respect your privacy
-								</FormHelperText>
-							</FormControl>
-							<FormControl className={classes.formControl}>
-								<TextField
-									id="phone"
-									type="phone"
-									label="Phone"
-									value={state.phone}
-									onChange={getTextChangeHandler("phone")}
-								/>
-								<FormHelperText>
-									Please include your country code
-								</FormHelperText>
-							</FormControl>
-							<FormControl className={classes.formControl}>
-								<Select
-									value={state.reason}
-									onChange={getTextChangeHandler("reason")}
-									displayEmpty
-									required
-								>
-									<MenuItem value="" disabled>
-										(select reason)
-									</MenuItem>
-									{contactReasons.map((reason) => (
-										<MenuItem
-											key={reason.value}
-											value={reason.value}
-										>
-											{reason.label}
-										</MenuItem>
-									))}
-								</Select>
-								<FormHelperText>
-									How can we help you?
-								</FormHelperText>
-							</FormControl>
-							<FormControl className={classes.formControl}>
-								<TextField
-									label="Message"
-									multiline
-									value={state.message}
-									id="message"
-									required
-									onChange={getTextChangeHandler("message")}
-								/>
-								<FormHelperText>
-									Please tell us more about why you are
-									contacting us
-								</FormHelperText>
-							</FormControl>
-						</div>
-						<Grid container direction="row" spacing={3}>
-							<Grid item xs="auto">
+			<Card elevation={1}>
+				<CardHeader
+					title={"Get in Touch"}
+					titleTypographyProps={{ variant: "h2", fontWeight: "400" }}
+				/>
+
+				<Divider />
+
+				<CardContent>
+					<Box component="form" sx={{ ...css.form }}>
+						<Grid container direction="row" spacing={2}>
+							<Grid item xs={12} sm={6}>
+								<FormControl fullWidth>
+									<TextField
+										required
+										label="Name"
+										id="name"
+										value={state.name}
+										onChange={getTextChangeHandler("name")}
+									/>
+									<FormHelperText>
+										So we can address you
+									</FormHelperText>
+								</FormControl>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<FormControl fullWidth>
+									<TextField
+										id="email"
+										type="email"
+										label="Email"
+										required
+										value={state.email}
+										onChange={getTextChangeHandler("email")}
+									/>
+									<FormHelperText>
+										We respect your privacy
+									</FormHelperText>
+								</FormControl>
+							</Grid>
+							<Grid item xs={12} sm={6}>
+								<FormControl fullWidth>
+									<TextField
+										id="phone"
+										type="phone"
+										label="Phone"
+										value={state.phone}
+										onChange={getTextChangeHandler("phone")}
+									/>
+									<FormHelperText>
+										(Optional) Please include your country
+										code
+									</FormHelperText>
+								</FormControl>
+							</Grid>
+							<Grid item xs={12} sm={7} sx={{ mt: 4 }}>
+								<FormControl fullWidth>
+									<InputLabel id="SelectReasonLabel">
+										Type of Inquiry
+									</InputLabel>
+									<Select
+										required
+										displayEmpty
+										labelId="SelectReasonLabel"
+										label="Type of Inquiry"
+										value={state.reason}
+										onChange={getTextChangeHandler(
+											"reason"
+										)}
+									>
+										{(contactReasons &&
+											contactReasons.map((reason) => (
+												<MenuItem
+													key={reason.value}
+													value={reason.value}
+												>
+													{reason.label}
+												</MenuItem>
+											))) || (
+											<>
+												<MenuItem value={1}>
+													General
+												</MenuItem>
+												<MenuItem value={2}>
+													Request a demo
+												</MenuItem>
+												<MenuItem value={3}>
+													Register an Account
+												</MenuItem>
+												<MenuItem value={4}>
+													Get Support
+												</MenuItem>
+												<MenuItem value={5}>
+													Report a Problem
+												</MenuItem>
+											</>
+										)}
+									</Select>
+									<FormHelperText>
+										How can we help you?
+									</FormHelperText>
+								</FormControl>
+							</Grid>
+							<Grid item xs={12} sm={9}>
+								<FormControl fullWidth>
+									<TextField
+										required
+										multiline
+										id="message"
+										label="Message"
+										rows={4}
+										value={state.message}
+										onChange={getTextChangeHandler(
+											"message"
+										)}
+									/>
+								</FormControl>
+							</Grid>
+							<Grid item xs={12}>
+								<Divider sx={{ my: 3 }} />
+							</Grid>
+							<Grid item xs={6}>
 								<Button
+									fullWidth
+									id="btnSubmit"
 									variant="contained"
-									color="primary"
 									disabled={disabled}
 									onClick={handleSubmit}
 								>
 									Submit
 								</Button>
 							</Grid>
-							<Grid item xs="auto">
+							<Grid item xs={"auto"}>
 								<Button
-									variant="contained"
-									color="secondary"
+									fullWidth
+									variant="text"
+									type="reset"
 									onClick={() => {
 										reset();
 									}}
 								>
-									Reset
+									Reset Fields
 								</Button>
 							</Grid>
+							<Grid item xs={12}>
+								<NotificationLine type="notice">
+									<p>
+										By clicking Submit, you agree to our{" "}
+										<Link to="pages/terms-of-use">
+											Terms of Use
+										</Link>
+										&nbsp; and our{" "}
+										<Link to="pages/privacy-policy">
+											Privacy Policy
+										</Link>
+									</p>
+								</NotificationLine>
+								<NotificationLine type="footnote">
+									<p>
+										This site is also protected by{" "}
+										<a href="https://www.google.com/recaptcha/">
+											reCAPTCHA
+										</a>{" "}
+										and the Google{" "}
+										<a href="https://policies.google.com/privacy">
+											Privacy Policy
+										</a>{" "}
+										and{" "}
+										<a href="https://policies.google.com/terms">
+											Terms of Service
+										</a>{" "}
+										apply.
+									</p>
+								</NotificationLine>
+							</Grid>
 						</Grid>
-
-						<NoteLine>
-							By clicking Submit, you agree to our{" "}
-							<Link to="pages/terms-of-use">Terms of Use</Link>
-							&nbsp; and our{" "}
-							<Link to="pages/privacy-policy">
-								Privacy Policy
-							</Link>
-						</NoteLine>
-						<small>
-							This site is also protected by reCAPTCHA and the
-							Google{" "}
-							<a href="https://policies.google.com/privacy">
-								Privacy Policy
-							</a>{" "}
-							and{" "}
-							<a href="https://policies.google.com/terms">
-								Terms of Service
-							</a>{" "}
-							apply.
-						</small>
-					</section>
-				</section>
-			</form>
+					</Box>
+				</CardContent>
+			</Card>
 		</>
 	);
 }

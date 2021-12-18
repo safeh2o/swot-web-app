282dd4b6068b429ae010c33ce8079145d81a8f17
bhekuwenza <bhekuwenza@gmail.com>
2021-12-17 21:08:48 -0500

    Decoupled external css for sx switch. micro css reset only kept

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom";
-import App from "./components/App";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store, persistor } from "./store";
 import AppContext from "./contexts/AppContext";
 import { PersistGate } from "redux-persist/integration/react";
+
+// App Root Component
+import App from "./components/App";
 
 if (typeof window !== "undefined") {
 	ReactDOM.render(

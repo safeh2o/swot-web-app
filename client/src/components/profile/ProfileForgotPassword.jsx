0a963c1d4f229bde6687541e2302aaaf6d072d80
Mohamed Moselhy <look4d@gmail.com>
2022-01-16 14:19:15 -0500

    UI changes; backend changes; sendgrid migration

--- client/src/components/profile/ProfileForgotPassword.jsx
+++ client/src/components/profile/ProfileForgotPassword.jsx
@@ -1,81 +1,77 @@
-import { useEffect, useRef, useState } from "react";
+import {
+	Box,
+	Button,
+	Divider,
+	Grid,
+	Paper,
+	TextField,
+	Typography,
+} from "@mui/material";
+import axios from "axios";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import useForm from "../../hooks/useForm";
+import { handleServerMessages } from "../../reducers/notifications";
 
-export default function ProfileForgotPassword(props) {
-	const form = useRef(null);
-	const [messages, setMessages] = useState({});
+export default function ProfileForgotPassword() {
+	const dispatch = useDispatch();
 
-	const handleSubmitResponse = (data) => {
-		setMessages(data.messages);
-	};
-
-	const handleChange = () => {
-		setMessages({});
-	};
-
-	useEffect(() => {
-		$(form.current).ajaxForm((data) => {
-			handleSubmitResponse(data);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		axios.post("/api/forgotpassword", state).then((res) => {
+			console.log(res.data);
+			dispatch(handleServerMessages(res.data?.messages));
 		});
-		return () => {
-			$(form.current).off();
-		};
-	}, []);
+	};
+
+	const { state, getTextChangeHandler } = useForm({ email: "" });
 
 	return (
 		<>
-			<h1 className="content-title">Forgot Password</h1>
-			<section className="content-window">
-				<header>
-					<div>Enter the email you're using for next steps.</div>
-				</header>
-				<section>
-					<form
-						ref={form}
-						role="form"
-						action="/forgotpassword"
-						method="post"
-					>
-						<div className="flex-group">
-							<div className="flex-group-item line">
-								<label
-									htmlFor="email"
-									className="control-label"
-								>
-									Enter Your Email
-								</label>
-								<div className="flex-group-wrapper">
-									<input
-										onChange={() => {
-											handleChange();
-										}}
-										className="form-control email"
-										id="email"
-										placeholder="mail@example.com"
-										name="email"
-										type="email"
-									/>
-								</div>
-							</div>
-						</div>
-					</form>
-				</section>
-			</section>
-
-			<section className="content-window">
-				<section>
-					<div className="submission-wrap">
-						<input
-							type="submit"
-							className="button blue"
-							value="Reset"
-						/>
-						<Link to="/signin" className="button reset">
-							<span>Log in</span>
-						</Link>
-					</div>
-				</section>
-			</section>
+			<Box component="form" onSubmit={handleSubmit}>
+				<Grid container direction="column" spacing={3}>
+					<Grid item>
+						<Typography variant="h3" color="darkgrey">
+							Forgot Password
+						</Typography>
+					</Grid>
+					<Grid item>
+						<Paper sx={(theme) => ({ padding: theme.spacing(4) })}>
+							<Typography variant="body1">
+								Enter the email you are using for next steps
+							</Typography>
+							<Divider sx={{ my: 2 }} />
+							<TextField
+								type="email"
+								autoComplete="email"
+								required
+								label="Email"
+								value={state.email}
+								onChange={getTextChangeHandler("email")}
+							/>
+						</Paper>
+					</Grid>
+					<Grid item>
+						<Paper sx={(theme) => ({ padding: theme.spacing(2) })}>
+							<Grid
+								container
+								spacing={3}
+								alignItems="center"
+								mx={0}
+							>
+								<Grid item>
+									<Button type="submit" variant="contained">
+										Submit
+									</Button>
+								</Grid>
+								<Grid item>
+									<Link to="/signin">LOG IN</Link>
+								</Grid>
+							</Grid>
+						</Paper>
+					</Grid>
+				</Grid>
+			</Box>
 		</>
 	);
 }
--- server/utils/emailer.js
+++ server/utils/emailer.js
@@ -1,20 +1,25 @@
+const mailgun = require("mailgun-js")({
+	apiKey: process.env.MAILGUN_API_KEY,
+	domain: process.env.MAILGUN_DOMAIN,
+});
 
+exports.emailAdmin = function (message) {
+	const data = {
+		from: process.env.FROM_EMAIL,
+		to: process.env.SUPPORT_EMAIL,
+		subject: "SWOT Application Message",
+		text: message,
+	};
 
-const mailgun = require('mailgun-js')({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN});
-
-exports.emailAdmin = function(message) {
-  const data = {
-    from: process.env.FROM_ADDRESS,
-    to: process.env.ADMIN_EMAIL,
-    subject: 'SWOT Application Message',
-    text: message
-  };
-
-  mailgun.messages().send(data, (error, body) => {
-    if (error) {
-      console.error("WARNING: ACTION NEEDED: Unable to send mail through Mailgun!", error, body);
-    } else {
-      console.log('Sent email:', body);
-    }
-  });
-}
+	mailgun.messages().send(data, (error, body) => {
+		if (error) {
+			console.error(
+				"WARNING: ACTION NEEDED: Unable to send mail through Mailgun!",
+				error,
+				body
+			);
+		} else {
+			console.log("Sent email:", body);
+		}
+	});
+};

74e82ef3d9f0b8cc4e8c3d6c59c8f9160e0cecff
bhekuwenza <bhekuwenza@gmail.com>
2022-05-26 00:02:44 -0400

    update nav component and styles

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,120 +1,75 @@
-import {
-	Box,
-	Card,
-	CardActions,
-	CardContent,
-	Divider,
-	Grid,
-	Typography,
-} from "@mui/material";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { Box, Typography } from "@mui/material";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { blogSelectors, getPosts } from "../reducers/posts";
 import { userSelectors } from "../reducers/user";
-import { Home as css } from "../styles/styles";
-import { IconArrowHome, IconProfile, IconSignIn } from "./icons"; // IconQuestionMark
-import Posts from "./Posts";
+import { IconArrowHome, IconEnvelope, IconSignIn, IconProfile } from "./icons";
 
 export default function Home() {
 	const isLoggedIn = useSelector(userSelectors.isLoggedIn);
 	const user = useSelector(userSelectors.user);
-	const posts = useSelector(blogSelectors.posts);
-	const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(getPosts());
-	}, []);
-
-	const privilegedContent = () => {
-		return (
-			<>
-				<Box sx={{ ...css.userPanel }}>
-					<Box sx={{ ...css.action, ...css.actionSwot }}>
+	return (
+		<>
+			<section id="home-profile">
+				<div
+					className={
+						(!isLoggedIn && "section-wrap compact") ||
+						"section-wrap"
+					}
+				>
+					<Box className="options">
 						{(!isLoggedIn && (
 							<>
-								<Box>
-									<IconProfile
-										sx={{
-											width: "2.5rem",
-											height: "2.5rem",
-										}}
-									/>
-									<Typography variant="h3" component={"p"}>
+								<Typography component={"h1"} variant="body1">
+									{isLoggedIn &&
+										"Welcome Back, " +
+											(user.name.first
+												? user.name.first
+												: "User")}
+								</Typography>
+								<Box className="option request">
+									<i>
+										<IconProfile />
+									</i>
+									<Typography component={"p"}>
 										<Link to="/contact">Request</Link> an
-										account for you or your team members.
+										account.
 									</Typography>
 								</Box>
-								<Box>
-									<IconSignIn
-										sx={{
-											width: "2.5rem",
-											height: "2.5rem",
-										}}
-									/>
-									<Typography variant="h3" component={"p"}>
+								<Box className="option">
+									<i>
+										<IconSignIn />
+									</i>
+									<Typography component={"p"}>
 										<Link to="/signin">Sign in</Link> to
-										begin or continue your SWOT Process.
+										your account.
 									</Typography>
 								</Box>
 							</>
 						)) || (
 							<>
-								<IconArrowHome
-									viewBox="0 0 256 256"
-									sx={{
-										width: "2.5rem",
-										height: "2.5rem",
-									}}
-								/>
-								<Typography variant="h3">
-									Use the Tool menu to begin or continue your
-									SWOT Process
-								</Typography>
+								<Box className="option">
+									<i>
+										<IconArrowHome />
+									</i>
+									<Typography variant="p">
+										Use the Tool menu to begin or continue
+										your SWOT Process
+									</Typography>
+								</Box>
+								<Box className="option">
+									<i>
+										<IconEnvelope />
+									</i>
+									<Typography component={"p"}>
+										<Link to="/contact">Contact Us</Link>
+									</Typography>
+								</Box>
 							</>
 						)}
 					</Box>
-					<Box sx={{ ...css.actionGuide }}>
-						<Typography component="p">
-							For help, <Link to="/contact">Contact Us</Link>
-						</Typography>
-					</Box>
-				</Box>
-			</>
-		);
-	};
-
-	return (
-		<>
-			<Typography
-				component={"h1"}
-				variant="body1"
-				sx={{ ...css.sectionHeader }}
-			>
-				{(!isLoggedIn && "Welcome, Guest") ||
-					"Welcome Back, " +
-						(user.name.first ? user.name.first : "User")}
-			</Typography>
-
-			{privilegedContent()}
-
-			<Typography
-				component={"h1"}
-				variant="body1"
-				sx={{ ...css.sectionHeader }}
-			>
-				Latest News
-			</Typography>
-
-			<Posts type={"news"} data={posts.slice(0, 3)} />
-			<Card sx={{ ...css.cardElement, ...css.news }}>
-				<CardActions className="more" sx={{ p: 2 }}>
-					<Divider sx={{ mb: 1 }} />
-					<Link to="/blog">
-						<Typography variant="body1">More News</Typography>
-					</Link>
-				</CardActions>
-			</Card>
+				</div>
+			</section>
 		</>
 	);
 }

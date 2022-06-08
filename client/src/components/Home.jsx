77c54ff6a16eb406835be8016b85107b6ebccbef
bhekuwenza <bhekuwenza@gmail.com>
2022-06-08 14:27:34 -0400

    home page mobile ui

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,8 @@
-import { Box, SvgIcon, Typography } from "@mui/material";
+import { SvgIcon, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { userSelectors } from "../reducers/user";
-import { IconArrowHome, IconEnvelope, IconSignIn, IconProfile } from "./icons";
+import { IconEnvelope, IconSignIn, IconProfile } from "./icons";
 
 export default function Home() {
 	const isLoggedIn = useSelector(userSelectors.isLoggedIn);
@@ -20,12 +20,81 @@ export default function Home() {
 					</h1>
 					{isLoggedIn && (
 						<>
-							<Box className="cards">
-								<Box className="card rte tool">
+							<div className="cards">
+								<div className="card rte tool">
 									<h2 className="title">Tool</h2>
 									<div className="text">
-										Use the Tool to begin or continue your
-										SWOT Process
+										<p>
+											Use the Tool to begin or continue
+											your SWOT Process;
+										</p>
+										<ol className="tool-menu-mobile con">
+											<li>
+												<NavLink to={"/"}>
+													Planning &amp; Data
+													Collection
+													<i>
+														<SvgIcon
+															xmlns="http://www.w3.org/2000/svg"
+															width="40px"
+															height="40px"
+															fill="#000000"
+															viewBox="0 0 256 256"
+														>
+															<path d="M221.7,133.7l-72,72A8.3,8.3,0,0,1,144,208a8.5,8.5,0,0,1-3.1-.6A8,8,0,0,1,136,200V136H40a8,8,0,0,1,0-16h96V56a8,8,0,0,1,4.9-7.4,8.4,8.4,0,0,1,8.8,1.7l72,72A8.1,8.1,0,0,1,221.7,133.7Z"></path>
+														</SvgIcon>
+													</i>
+												</NavLink>
+											</li>
+											<li>
+												<NavLink to={"/"}>
+													Uploading Data
+													<i>
+														<SvgIcon
+															xmlns="http://www.w3.org/2000/svg"
+															width="40px"
+															height="40px"
+															fill="#000000"
+															viewBox="0 0 256 256"
+														>
+															<path d="M221.7,133.7l-72,72A8.3,8.3,0,0,1,144,208a8.5,8.5,0,0,1-3.1-.6A8,8,0,0,1,136,200V136H40a8,8,0,0,1,0-16h96V56a8,8,0,0,1,4.9-7.4,8.4,8.4,0,0,1,8.8,1.7l72,72A8.1,8.1,0,0,1,221.7,133.7Z"></path>
+														</SvgIcon>
+													</i>
+												</NavLink>
+											</li>
+											<li>
+												<NavLink to={"/"}>
+													Running Your Analysis
+													<i>
+														<SvgIcon
+															xmlns="http://www.w3.org/2000/svg"
+															width="40px"
+															height="40px"
+															fill="#000000"
+															viewBox="0 0 256 256"
+														>
+															<path d="M221.7,133.7l-72,72A8.3,8.3,0,0,1,144,208a8.5,8.5,0,0,1-3.1-.6A8,8,0,0,1,136,200V136H40a8,8,0,0,1,0-16h96V56a8,8,0,0,1,4.9-7.4,8.4,8.4,0,0,1,8.8,1.7l72,72A8.1,8.1,0,0,1,221.7,133.7Z"></path>
+														</SvgIcon>
+													</i>
+												</NavLink>
+											</li>
+											<li>
+												<NavLink to={"/"}>
+													Obtain Results
+													<i>
+														<SvgIcon
+															xmlns="http://www.w3.org/2000/svg"
+															width="40px"
+															height="40px"
+															fill="#000000"
+															viewBox="0 0 256 256"
+														>
+															<path d="M221.7,133.7l-72,72A8.3,8.3,0,0,1,144,208a8.5,8.5,0,0,1-3.1-.6A8,8,0,0,1,136,200V136H40a8,8,0,0,1,0-16h96V56a8,8,0,0,1,4.9-7.4,8.4,8.4,0,0,1,8.8,1.7l72,72A8.1,8.1,0,0,1,221.7,133.7Z"></path>
+														</SvgIcon>
+													</i>
+												</NavLink>
+											</li>
+										</ol>
 									</div>
 									<i>
 										<SvgIcon
@@ -38,8 +107,8 @@ export default function Home() {
 											<path d="M224,128a8,8,0,0,1-8,8H120v64a8,8,0,0,1-4.9,7.4,8.5,8.5,0,0,1-3.1.6,8.3,8.3,0,0,1-5.7-2.3l-72-72a8.1,8.1,0,0,1,0-11.4l72-72a8.4,8.4,0,0,1,8.8-1.7A8,8,0,0,1,120,56v64h96A8,8,0,0,1,224,128Z"></path>
 										</SvgIcon>
 									</i>
-								</Box>
-								<Box className="card rte">
+								</div>
+								<div className="card rte">
 									<h2 className="title">Quickstart Guide</h2>
 									<div className="text">
 										Overview of the SWOT, from data
@@ -56,8 +125,8 @@ export default function Home() {
 											<path d="M221.7,133.7l-72,72A8.3,8.3,0,0,1,144,208a8.5,8.5,0,0,1-3.1-.6A8,8,0,0,1,136,200V136H40a8,8,0,0,1,0-16h96V56a8,8,0,0,1,4.9-7.4,8.4,8.4,0,0,1,8.8,1.7l72,72A8.1,8.1,0,0,1,221.7,133.7Z"></path>
 										</SvgIcon>
 									</i>
-								</Box>
-								<Box className="card rte">
+								</div>
+								<div className="card rte">
 									<h2 className="title">Technical Blogs</h2>
 									<div className="text">
 										In-depth information on reading your
@@ -75,8 +144,8 @@ export default function Home() {
 											<path d="M221.7,133.7l-72,72A8.3,8.3,0,0,1,144,208a8.5,8.5,0,0,1-3.1-.6A8,8,0,0,1,136,200V136H40a8,8,0,0,1,0-16h96V56a8,8,0,0,1,4.9-7.4,8.4,8.4,0,0,1,8.8,1.7l72,72A8.1,8.1,0,0,1,221.7,133.7Z"></path>
 										</SvgIcon>
 									</i>
-								</Box>
-								<Box className="card rte">
+								</div>
+								<div className="card rte">
 									<h2 className="title">FAQ&lsquo;s</h2>
 									<div className="text">
 										Frequently asked questions
@@ -92,14 +161,14 @@ export default function Home() {
 											<path d="M221.7,133.7l-72,72A8.3,8.3,0,0,1,144,208a8.5,8.5,0,0,1-3.1-.6A8,8,0,0,1,136,200V136H40a8,8,0,0,1,0-16h96V56a8,8,0,0,1,4.9-7.4,8.4,8.4,0,0,1,8.8,1.7l72,72A8.1,8.1,0,0,1,221.7,133.7Z"></path>
 										</SvgIcon>
 									</i>
-								</Box>
-							</Box>
+								</div>
+							</div>
 						</>
 					)}
-					<Box className="options">
+					<div className="options">
 						{(!isLoggedIn && (
 							<>
-								<Box className="option request">
+								<div className="option request">
 									<i>
 										<IconProfile />
 									</i>
@@ -107,31 +176,31 @@ export default function Home() {
 										<Link to="/contact">Request</Link> an
 										account.
 									</Typography>
-								</Box>
-								<Box className="option">
+								</div>
+								<div className="option">
 									<i>
 										<IconSignIn />
 									</i>
-									<Typography component={"p"}>
+									<p>
 										<Link to="/signin">Sign in</Link> to
 										your account.
-									</Typography>
-								</Box>
+									</p>
+								</div>
 							</>
 						)) || (
 							<>
-								<Box className="option contact">
+								<div className="option contact">
 									<i>
 										<IconEnvelope />
 									</i>
-									<Typography component={"p"}>
+									<p>
 										Need help?{" "}
 										<Link to="/contact">Contact Us</Link>
-									</Typography>
-								</Box>
+									</p>
+								</div>
 							</>
 						)}
-					</Box>
+					</div>
 				</div>
 			</section>
 		</>

013523196a08e8f8a4b64f68db52a870efbaa7d6
Mohamed Moselhy <look4d@gmail.com>
2022-11-07 23:12:22 -0500

    revert notification line; removed launch banner

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -231,27 +231,6 @@ export default function Header() {
 
 	return (
 		<>
-			{/* <Grid
-				container
-				display="flex"
-				justifyContent="center"
-				mt={2}
-				sx={{ backgroundColor: "lightyellow" }}
-				py={2}
-			>
-				<Grid>
-					<Typography>
-						We are launching our new and improved Safe Water
-						Optimization Tool Version 2 on November 8th, 2022. To
-						learn more and register for our launch event,{" "}
-						<Link href="https://www.yorku.ca/dighr/events/swot-v2-launch/">
-							click here
-						</Link>
-						.
-					</Typography>
-				</Grid>
-			</Grid> */}
-
 			<nav role="navigation">
 				<NavLink to={"/"} className="menu-brand">
 					<SWOTLogo className="large" />

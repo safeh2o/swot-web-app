3cbfb23e139b50944d06b54f0322443efaeaa611
bhekuwenza <bhekuwenza@gmail.com>
2023-05-29 17:24:20 -0400

    get out icon

--- client/src/components/icons/IconSignOut.jsx
+++ client/src/components/icons/IconSignOut.jsx
@@ -5,11 +5,9 @@ function IconSignOut(props) {
 		<SvgIcon viewBox="0 0 32 32" {...props}>
 			<path fill="none" d="M0 0h32v32H0z" />
 			<path
-				fill="none"
-				strokeWidth="2"
-				stroke="currentColor"
-				d="M21.8 10.8 27 16l-5.2 5.3M13 16h14M13 27H6c-.6 0-1-.4-1-1V6c0-.6.4-1 1-1h7"
-			/>
+				fill="currentColor"
+				d="M18.5 27c0 .8-.7 1.5-1.5 1.5H7c-1.4 0-2.5-1.1-2.5-2.5V6c0-1.4 1.1-2.5 2.5-2.5h10c.8 0 1.5.7 1.5 1.5s-.7 1.5-1.5 1.5H7.5v19H17c.8 0 1.5.7 1.5 1.5zm10.2-11.7l-5-5c-.2-.2-.4-.3-.7-.3-.6 0-1 .4-1 1v3.5h-8c-.8 0-1.5.7-1.5 1.5s.7 1.5 1.5 1.5h8V21c0 .3.1.5.3.7.4.4 1 .4 1.4 0l5-5c.4-.4.4-1 0-1.4z"
+			></path>
 		</SvgIcon>
 	);
 }

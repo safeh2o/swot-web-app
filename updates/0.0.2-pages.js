9de698e5ebc2016ad5dab186d903dcb04e6c7094
Mohamed Moselhy <look4d@gmail.com>
2023-10-02 07:05:22 -0400

    policy -> notice

--- updates/0.0.2-pages.js
+++ updates/0.0.2-pages.js
@@ -1,10 +1,30 @@
 exports.create = {
 	Page: [
-    { 'title': 'How It Works', 'state': 'published', 'content.extended': 'How it works'},
-    { 'title': 'Terms Of Use', 'state': 'published', 'content.extended': 'Terms Of Use'},
-    { 'title': 'Privacy Policy', 'state': 'published', 'content.extended': 'Privacy Policy'},
-    { 'title': 'Announcements', 'state': 'published', 'content.extended': 'Announcements'},
-    { 'title': 'Instructions', 'state': 'published', 'content.extended': 'Instructions'},
-    { 'title': 'FAQs', 'state': 'published', 'content.extended': 'FAQs'},
+		{
+			title: "How It Works",
+			state: "published",
+			"content.extended": "How it works",
+		},
+		{
+			title: "Terms Of Use",
+			state: "published",
+			"content.extended": "Terms Of Use",
+		},
+		{
+			title: "Privacy Notice",
+			state: "published",
+			"content.extended": "Privacy Notice",
+		},
+		{
+			title: "Announcements",
+			state: "published",
+			"content.extended": "Announcements",
+		},
+		{
+			title: "Instructions",
+			state: "published",
+			"content.extended": "Instructions",
+		},
+		{ title: "FAQs", state: "published", "content.extended": "FAQs" },
 	],
 };

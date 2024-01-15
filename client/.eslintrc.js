a081db1c23620e0907a3ae43b7fcf70e4d82a323
Mohamed Moselhy <look4d@gmail.com>
2024-01-15 13:05:26 -0500

    react-hooks eslint plugin

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -24,8 +24,10 @@ module.exports = {
 		ecmaVersion: "latest",
 		sourceType: "module",
 	},
-	plugins: ["@typescript-eslint", "react"],
+	plugins: ["@typescript-eslint", "react", "react-hooks"],
 	rules: {
 		"react/react-in-jsx-scope": "off",
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
 	},
 };
